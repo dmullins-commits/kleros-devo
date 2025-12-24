6e4bba3a2f79c3fb9d73cb80bb641a640b1187f0
dmullins <dmullins@616athletics.com>
2025-12-24 18:21:45 +0000

    File changes

--- src/pages.config.js
+++ src/pages.config.js
@@ -1,47 +1,31 @@
 import Athletes from './pages/Athletes';
-import CleanupClinton from './pages/CleanupClinton';
-import CleanupUnknownTeams from './pages/CleanupUnknownTeams';
 import Dashboard from './pages/Dashboard';
 import DataMigration from './pages/DataMigration';
-import DataMigrationOrg from './pages/DataMigrationOrg';
-import DiagnoseDates from './pages/DiagnoseDates';
-import FixDatePadding from './pages/FixDatePadding';
-import FixUnknownAthletes from './pages/FixUnknownAthletes';
 import Home from './pages/Home';
 import LeaderboardBuilder from './pages/LeaderboardBuilder';
 import ManageUsers from './pages/ManageUsers';
 import Metrics from './pages/Metrics';
-import MigrateTeamsClassPeriods from './pages/MigrateTeamsClassPeriods';
 import ProgressTracking from './pages/ProgressTracking';
 import Reports from './pages/Reports';
 import TestingCenter from './pages/TestingCenter';
 import Upload from './pages/Upload';
 import Workouts from './pages/Workouts';
-import CleanupUnknownAthletes from './pages/CleanupUnknownAthletes';
 import __Layout from './Layout.jsx';
 
 
 export const PAGES = {
     "Athletes": Athletes,
-    "CleanupClinton": CleanupClinton,
-    "CleanupUnknownTeams": CleanupUnknownTeams,
     "Dashboard": Dashboard,
     "DataMigration": DataMigration,
-    "DataMigrationOrg": DataMigrationOrg,
-    "DiagnoseDates": DiagnoseDates,
-    "FixDatePadding": FixDatePadding,
-    "FixUnknownAthletes": FixUnknownAthletes,
     "Home": Home,
     "LeaderboardBuilder": LeaderboardBuilder,
     "ManageUsers": ManageUsers,
     "Metrics": Metrics,
-    "MigrateTeamsClassPeriods": MigrateTeamsClassPeriods,
     "ProgressTracking": ProgressTracking,
     "Reports": Reports,
     "TestingCenter": TestingCenter,
     "Upload": Upload,
     "Workouts": Workouts,
-    "CleanupUnknownAthletes": CleanupUnknownAthletes,
 }
 
 export const pagesConfig = {
